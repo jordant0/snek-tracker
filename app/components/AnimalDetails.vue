680b8852f28ed5667e67dce47181ab1d500363a5
Jordan Tran <kim.tran94@gmail.com>
2019-01-22 22:05:47 -0600

    Move add event drawer to right

--- app/components/AnimalDetails.vue
+++ app/components/AnimalDetails.vue
@@ -29,48 +29,40 @@
 <template>
   <Page class="page">
     <ActionBar class="action-bar">
-      <GridLayout width="100%" columns="auto, *">
-        <Label class="icon" :text="String.fromCharCode(0xf278)" @tap="$refs.drawer.nativeView.showDrawer()" col="0" />
+      <GridLayout width="100%" columns="auto, *, auto">
+        <Label class="icon" :text="String.fromCharCode(0xf2fa)" @tap="$navigateBack" col="0" />
         <Label class="title" :text="animal.name"  col="1"/>
+        <Label class="icon" :text="String.fromCharCode(0xf278)" @tap="$refs.drawer.nativeView.showDrawer()" col="2" paddingLeft="10" />
       </GridLayout>
     </ActionBar>
 
-    <RadSideDrawer ref="drawer">
-      <FlexboxLayout ~drawerContent class="sidedrawer-content">
-        <StackLayout>
-          <Label class="sidedrawer-header" text="Add Event"/>
-
-          <StackLayout class="sidedrawer-list-item" orientation="horizontal">
-            <Label class="icon" :text="String.fromCharCode(0xf153)" />
-            <Label class="title" text="Feeding" />
-          </StackLayout>
-
-          <StackLayout class="sidedrawer-list-item" orientation="horizontal">
-            <Label class="icon" :text="String.fromCharCode(0xf207)" />
-            <Label class="title" text="Handling" />
-          </StackLayout>
-
-          <StackLayout class="sidedrawer-list-item" orientation="horizontal">
-            <Label class="icon" :text="String.fromCharCode(0xf1bb)" />
-            <Label class="title" text="Weight" />
-          </StackLayout>
-
-          <StackLayout class="sidedrawer-list-item" orientation="horizontal">
-            <Label class="icon" :text="String.fromCharCode(0xf254)" />
-            <Label class="title" text="Shedding" />
-          </StackLayout>
-
-          <StackLayout class="sidedrawer-list-item" orientation="horizontal">
-            <Label class="icon" :text="String.fromCharCode(0xf27d)" />
-            <Label class="title" text="Other" />
-          </StackLayout>
+    <RadSideDrawer ref="drawer" drawerLocation="Right">
+      <StackLayout ~drawerContent class="sidedrawer-content">
+        <StackLayout class="sidedrawer-list-item" orientation="horizontal">
+          <Label class="icon" :text="String.fromCharCode(0xf153)" />
+          <Label class="title" text="Feeding" />
         </StackLayout>
 
-        <StackLayout class="sidedrawer-list-item" orientation="horizontal" flexShrink="0">
-          <Label class="icon" :text="String.fromCharCode(0xf2fa)" />
-          <Label class="title" text="Back" @tap="$navigateBack" />
+        <StackLayout class="sidedrawer-list-item" orientation="horizontal">
+          <Label class="icon" :text="String.fromCharCode(0xf207)" />
+          <Label class="title" text="Handling" />
         </StackLayout>
-      </FlexboxLayout>
+
+        <StackLayout class="sidedrawer-list-item" orientation="horizontal">
+          <Label class="icon" :text="String.fromCharCode(0xf1bb)" />
+          <Label class="title" text="Weight" />
+        </StackLayout>
+
+        <StackLayout class="sidedrawer-list-item" orientation="horizontal">
+          <Label class="icon" :text="String.fromCharCode(0xf254)" />
+          <Label class="title" text="Shedding" />
+        </StackLayout>
+
+        <StackLayout class="sidedrawer-list-item" orientation="horizontal">
+          <Label class="icon" :text="String.fromCharCode(0xf27d)" />
+          <Label class="title" text="Other" />
+        </StackLayout>
+      </StackLayout>
 
       <StackLayout ~mainContent>
         <Label :text="animal.name" />
